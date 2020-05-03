84c7f97487972d22f65fc3fb962e80e96483a19e
Jayme Gordon <jaymegordo@gmail.com>
2020-05-03 10:50:55 -0700

    Buillding fleet monthly summary report

--- smseventlog/reports.py
+++ smseventlog/reports.py
@@ -7,6 +7,8 @@ from weasyprint import HTML
 
 from . import functions as f
 from . import units as un
+from . import queries as qr
+from .database import db
 from .__init__ import *
 
 global p_reports
@@ -35,21 +37,28 @@ def format_dtype(df, formats):
 
 def set_style(df):
     # Dataframe general column alignment/number formatting
-    # Mask of numeric columns
-    numeric_col_mask = df.dtypes.apply(lambda x: issubclass(np.dtype(x).type, np.number))
+    numeric_mask = df.dtypes.apply(lambda x: issubclass(np.dtype(x).type, np.number))
+    date_mask = df.dtypes.apply(lambda x: issubclass(np.dtype(x).type, np.datetime64))
 
     # Center table headers
-    d = dict(
+    s = []
+    s.append(dict(
         selector='th',
-        props=[('text-align', 'center')])
+        props=[('text-align', 'center'), ('background', '#265474')]))
+    s.append(dict(
+        selector='table, th, td',
+        props=[('border', '1px solid #000000'), ('padding', '3px 5px')]))
 
     # Style
     style = df.style \
-        .set_properties(subset=df.columns[numeric_col_mask], **{'text-align':'right'})\
-        .set_properties(subset=df.columns[~numeric_col_mask], **{'text-align':'center'})\
-        .format(lambda x: '{:,.0f}'.format(x) if x > 1e3 else '{:,.2f}'.format(x), # format numeric values
-                    subset=pd.IndexSlice[:, df.columns[numeric_col_mask]])\
-        .set_table_styles([d])
+        .set_properties(subset=df.columns[numeric_mask], **{'text-align':'right'})\
+        .set_properties(subset=df.columns[~numeric_mask], **{'text-align':'left'})\
+        .set_properties(subset=df.columns[date_mask], **{'text-align':'center'})\
+        .format(lambda x: '{:,.0f}'.format(x) if x > 1e3 else '{:,.2f}'.format(x), # default number format
+                    subset=pd.IndexSlice[:, df.columns[numeric_mask]])\
+        .set_table_styles(s) \
+        .set_table_attributes('style="border-collapse: collapse"') \
+        .set_na_rep('')
     
     return style
 
@@ -59,16 +68,18 @@ def report_unit_hrs_monthly(month):
     template = env.get_template('report_template.html')
 
     d = dt(dt.now().year, month, 1)
-    title = 'Fort Hills Monthly SMR - {}'.format(dt.strftime('%Y-%m'))
+    title = 'Fort Hills Monthly SMR - {}'.format(d.strftime('%Y-%m'))
     df = un.df_unit_hrs_monthly(month=month)
 
     style = set_style(df)
 
-    formats = dict(int64='{:,}')
+    # specific number formats
+    formats = {'int64': '{:,}', 'datetime64[ns]': '{:%Y-%m-%d}'}
     m = format_dtype(df=df, formats=formats)
     style.format(m)
     
     style.set_properties(subset=['Unit'], **{'font-weight': 'bold'})
+    style.set_properties(subset=['Unit', 'Serial'], **{'text-align':'center'})
 
     html_tbl = style.hide_index().render()
     template_vars = {'title' : title,
@@ -85,15 +96,57 @@ def report_unit_hrs_monthly(month):
     p = p_base / f'{title}.csv'
     df.to_csv(p)
 
+
+class Report(object):
+    def __init__(self):
+        # dict of {df_name: {func: func_definition, kw: **kw, df=None}}
+        dfs = {}
+
+        f.set_self(self, vars())
+
+    def add_df(self, name, func, kw):
+        self.dfs[name] = dict(func=func, kw=kw, df=None)
+
+    def load_all_dfs(self):
+        # call each df's function with its args and assign to df
+        for name in self.dfs:
+            self.load_df(name=name)
+
+    def load_df(self, name):
+        # load df from either function defn or query obj
+        m = self.dfs[name]
+        func, kw = m['func'], m['kw']
+
+        if issubclass(func.__class__, qr.QueryBase):
+            m['df'] = db.get_df(query=func, **kw)
+        else:
+            m['df'] = func(**kw)
+
+    def print_dfs(self):
+        for k in self.dfs:
+            m = self.dfs[k]
+            rows = 0 if m['df'] is None else len(m['df'])
+            print('{}: {}, {}, {}'.format(k, rows, ' '.join(str(m['func']).split(' ')[:2]), m['kw']))
+
+    def get_all_dfs(self):
+        # TODO: could probably use a filter here
+        return [m['df'] for m in self.dfs.values() if not m['df'] is None]            
+    
+class FleetMonthlyReport(Report):
+    def __init__(self):
+        super().__init__()
+        
+        month = 4
+        self.add_df(name='unit_smr', func=un.df_unit_hrs_monthly, kw=dict(month=month))
+        self.add_df(name='FC_Summary', func=qr.FCSummary(), kw=dict(default=True))
+
+    
+
 def get_df():
     # get raw df
-
     # process/shape?
-
     # style
-
-    # set col formats
-    
+    # set col formats   
     return
 
 # Fleet Monthly Report
