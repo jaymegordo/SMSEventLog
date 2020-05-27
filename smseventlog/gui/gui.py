70f9400c04f680555e7b2b175b869142bc6a5ab4
Jayme Gordon <jaymegordo@gmail.com>
2020-05-27 08:41:43 -0700

    removed table data model, minor name changes

--- smseventlog/gui/gui.py
+++ smseventlog/gui/gui.py
@@ -1,4 +1,3 @@
-
 from . import tables as tbls
 from .__init__ import *
 from . import dialogs as dlgs
@@ -21,152 +20,12 @@ minesite, customer = 'FortHills', 'Suncor'
 # TODO: Filter rows
 # TODO: green 'flash' for user confirmation value updated in db
 # TODO: Show 'details' menu > QListView?
-# TODO: change minesite > ctrl+shift+M
 
 
-
-class Table(QAbstractTableModel):
-    def __init__(self, df, parent=None):
-        QAbstractTableModel.__init__(self, parent)
-        title = parent.title
-        tablename = f.config['TableName']['Update'][title] # name of db model
-        dbtable = getattr(dbm, tablename) # db model definition, NOT instance
-        
-        _cols = list(df.columns)
-        r, c = df.shape
-        
-        # create tuple of ints from parent's list of disabled table headers
-        disabled_cols = tuple(i for i, col in enumerate(_cols) if col in parent.disabled_cols)
-        
-        # tuple of ints for date cols
-        dt_cols = tuple(i for i, val in enumerate(df.dtypes) if val == 'datetime64[ns]')
-
-        f.set_self(self, vars())
-
-    def insertRows(self, m, parent=None):
-        rows = self.rowCount()
-        self.beginInsertRows(QModelIndex(), rows, rows) # parent, first, last
-        self.df = self.df.append(m, ignore_index=True)
-        self.endInsertRows()
-
-    def removeRows(self, i, parent=None):
-        self.beginRemoveRows(QModelIndex(), i, i) # parent, row, count?
-        df = self.df
-        self.df = df.drop(df.index[i]).reset_index(drop=True)
-        self.endInsertRows()
-
-        # return super().removeRows(self, int, parent=parent)
-
-    def rowCount(self, index=QModelIndex()):
-        return self.df.shape[0]
-
-    def columnCount(self, index=QModelIndex()):
-        return self.df.shape[1]
-
-    def data(self, index, role=Qt.DisplayRole):
-        # TableView asks the model for data to display or edit
-        df = self.df
-
-        if index.isValid():
-            row, col = self.getRowCol(index)
-            val = df.iloc[row, col]
-
-            if role in (Qt.DisplayRole, Qt.EditRole):
-                if col in self.dt_cols:
-                    return val
-                elif not pd.isnull(val):
-                    return str(val)
-                else:
-                    return ''
-
-        return None
-
-    def getColIndex(self, header):
-        return self.df.columns.get_loc(header)
-    
-    def getRowCol(self, index):
-        return index.row(), index.column()
-
-    def update_db(self, index, val):
-        # Update single value from row in database
-        # TODO: this could maybe move to TableWidget?
-        row, col = index.row(), index.column()
-        df, parent = self.df, self.parent
-        header = df.columns[col] # view header
-
-        # print(row, df.iloc[row, col], df.iloc[row, 0], df.index[row])
-        db_col_map = parent.db_col_map
-        dbtable = self.dbtable if not header in db_col_map else getattr(dbm, db_col_map[header])
-
-        check_exists = False if not header in parent.check_exist_cols else True
-
-        e = el.Row(tbl=self, dbtable=dbtable, i=row)
-        e.update_single(header=header, val=val, check_exists=check_exists)
-    
-    def create_model(self, i):
-        # Not used currently
-        # maybe don't need this, have to create object from database usually?
-
-        # create dbmodel from table model given row index i
-        e = self.dbtable()
-        df = self.df
-        view_cols = self._cols
-        model_cols = f.convert_list_view_db(title=self.title, cols=view_cols)
-
-        # loop cols, setattr on model
-        for col, v in enumerate(model_cols):
-            setattr(e, v, df.iloc[i, col])
-        
-        return e
-               
-    def setData(self, index, val, role=Qt.EditRole):
-        if not index.isValid() or role != Qt.EditRole:
-            return False
-        
-        row, col = self.getRowCol(index)
-        df = self.df
-        df.iloc[row, col] = val
-
-        self.update_db(index=index, val=val)
-        self.dataChanged.emit(index, index)
-
-        return True
-
-    def headerData(self, p_int, orientation, role):
-        cols = self._cols
-
-        if role == Qt.DisplayRole:
-            if orientation == Qt.Horizontal:
-                if p_int < len(cols):
-                    return cols[p_int]
-                else:
-                    return ''
-            elif orientation == Qt.Vertical:
-                return p_int
-
-        return None
-
-    def sort(self, col, order):
-        self.layoutAboutToBeChanged.emit()
-
-        self.df.sort_values( 
-            self._cols[col],
-            ascending=order==Qt.AscendingOrder, inplace=True)
-
-        self.layoutChanged.emit()
-
-    def flags(self, index):
-        ans = Qt.ItemIsEnabled | Qt.ItemIsSelectable 
-        # Qt.ItemIsEditable ?
-        # fl |= Qt.ItemIsDragEnabled
-        # fl |= Qt.ItemIsDropEnabled
-
-        if not index.column() in self.disabled_cols:
-            ans |= Qt.ItemIsEditable
-        
-        return ans
 
 class MainWindow(QMainWindow):
+    minesite_changed = pyqtSignal(str)
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle(title)
@@ -176,8 +35,10 @@ class MainWindow(QMainWindow):
         s = QSettings('sms', 'smseventlog')
         self.resize(s.value('window size', defaultValue=QSize(1200, 1000)))
         self.move(s.value('window position', defaultValue=QPoint(50, 50)))
-        self.minesite = s.value('minesite', defaultValue='FortHills')
+        self._minesite = s.value('minesite', defaultValue='FortHills')
         self.settings = s
+
+        self.minesite_changed.connect(self.update_statusbar)
 
         self.create_actions()
         self.create_menu()
@@ -185,29 +46,51 @@ class MainWindow(QMainWindow):
         tabs = TabWidget(self)
         self.setCentralWidget(tabs)
         tabs.setCurrentIndex(tabs.get_index(title=s.value('active table', 'Event Log')))
+        self.update_statusbar()
 
         self.tabs = tabs
-
-        # TODO: connect minesite_changed function
-        self.statusBar().showMessage(f'Minesite: {self.minesite}')
     
+    @property
+    def minesite(self):
+        return self._minesite
+
+    @minesite.setter
+    def minesite(self, val):
+        self._minesite = val
+        self.minesite_changed.emit(val)
+    
+    def update_statusbar(self):
+        self.statusBar().showMessage(f'Minesite: {self.minesite}')
+
     def after_init(self):
         # TODO: need to show window first, then show loading message
         self.username = self.get_username()
-        self.active_table().refresh(default=True)
+        self.active_table_widget().refresh(default=True)
+    
+    def active_table_widget(self):
+        return self.tabs.currentWidget()
     
     def active_table(self):
-        return self.tabs.currentWidget()
+        return self.active_table_widget().view
 
     def show_refresh(self):
-        self.active_table().show_refresh()
+        self.active_table_widget().show_refresh()
+    
+    def show_changeminesite(self):
+        try:
+            dlg = dlgs.ChangeMinesite(parent=self)
+            return dlg.exec_()
+        except:
+            msg = 'couldn\'t change minesite'
+            f.send_error(msg)
+            log.error(msg)
         
     def closeEvent(self, event):
         s = self.settings
         s.setValue('window size', self.size())
         s.setValue('window position', self.pos())
         s.setValue('minesite', self.minesite)
-        s.setValue('active table', self.active_table().title)
+        s.setValue('active table', self.active_table_widget().title)
     
     def get_username(self):
         s = self.settings
@@ -247,6 +130,8 @@ class MainWindow(QMainWindow):
         file_ = bar.addMenu('File')
         file_.addAction('New item')
         file_.addAction(self.act_refresh)
+        file_.addAction(self.act_change_minesite)
+        file_.addAction(self.act_viewfolder)
 
         edit_ = bar.addMenu('Edit')
         edit_.addAction('Edit item')
@@ -258,31 +143,39 @@ class MainWindow(QMainWindow):
         rows_.addAction(self.act_open_tsi)
         rows_.addAction(self.act_remove_tsi)
         rows_.addAction(self.act_delete_event)
+        rows_.addAction(self.act_update_component)
 
     def create_actions(self):
         # Menu/shortcuts
-        self.act_username = QAction('Reset Username', self, triggered=self.set_username)
+        act_username = QAction('Reset Username', self, triggered=self.set_username)
 
-        self.act_refresh = QAction('Refresh Menu', self, triggered=self.show_refresh)
-        self.act_refresh.setShortcut(QKeySequence('Ctrl+R'))
+        act_refresh = QAction('Refresh Menu', self, triggered=self.show_refresh)
+        act_refresh.setShortcut(QKeySequence('Ctrl+R'))
 
-        self.act_open_tsi = QAction('Open TSI', self, triggered=self.open_tsi)
-        self.act_remove_tsi = QAction('Remove TSI', self, triggered=self.remove_tsi)
-        self.act_delete_event = QAction('Delete Event', self, triggered=self.delete_event)
+        act_change_minesite = QAction('Change MineSite', self, triggered=self.show_changeminesite)
+        act_change_minesite.setShortcut(QKeySequence('Ctrl+Shift+M'))
+
+        act_open_tsi = QAction('Open TSI', self, triggered=self.open_tsi)
+        act_remove_tsi = QAction('Remove TSI', self, triggered=self.remove_tsi)
+        act_delete_event = QAction('Delete Event', self, triggered=self.delete_event)
 
         # TODO: only add these to context menu with specific tables, eg not FC Summary?
-        t = self.active_table
-        self.act_refresh_allopen = QAction('Refresh All Open', self, 
+        t = self.active_table_widget
+        act_refresh_allopen = QAction('Refresh All Open', self, 
             triggered=lambda: t().refresh_allopen(default=True))
-        self.act_refresh_allopen.setShortcut(QKeySequence('Ctrl+Shift+R'))
+        act_refresh_allopen.setShortcut(QKeySequence('Ctrl+Shift+R'))
         
-        self.act_refresh_lastweek = QAction('Refresh Last Week', self, 
+        act_refresh_lastweek = QAction('Refresh Last Week', self, 
             triggered=lambda: t().refresh_lastweek(default=True))
-        self.act_refresh_lastmonth = QAction('Refresh Last Month', self, 
+        act_refresh_lastmonth = QAction('Refresh Last Month', self, 
             triggered=lambda: t().refresh_lastmonth(default=True))
 
-        self.act_viewfolder = QAction('View Folder', self, triggered=self.view_folder)
-        self.act_viewfolder.setShortcut(QKeySequence('Ctrl+V'))
+        act_viewfolder = QAction('View Folder', self, triggered=self.view_folder)
+        act_viewfolder.setShortcut(QKeySequence('Ctrl+Shift+V'))
+
+        act_update_component = QAction('Update Component', self, triggered=lambda: t().show_component())
+
+        f.set_self(self, vars())
 
     def contextMenuEvent(self, event):
         # TODO: use caller to check current tab?
@@ -302,24 +195,24 @@ class MainWindow(QMainWindow):
         action = menu.exec_(self.mapToGlobal(event.pos()))
 
     def open_tsi(self):
-        table = self.active_table()
+        view = self.active_table()
     
-        if not table.title in ('Event Log', 'Work Orders'):
+        if not view.parent.title in ('Event Log', 'Work Orders', 'Component CO'):
             msg = 'Please chose a row from the Event Log or Work Orders tab.'
             dlgs.msg_simple(msg=msg, icon='warning')
             return
 
-        row = table.row_from_activerow()
+        row = view.row_from_activerow()
         if row is None: return
         row.update(vals=dict(StatusTSI='Open', TSIAuthor=self.username))
         # TODO: maybe show status message to confirm TSI opened?
 
     def remove_tsi(self):
-        table = self.active_table()
+        view = self.active_table()
         
-        if table.title == 'TSI':
-            e = table.model_from_activerow()
-            row = table.row_from_activerow()
+        if view.parent.title == 'TSI':
+            e = view.model_from_activerow()
+            row = view.row_from_activerow()
             if row is None: return
 
             # TODO: bit sketch, should give model to row first then access from dict only?
@@ -333,9 +226,9 @@ class MainWindow(QMainWindow):
     
     def delete_event(self):
         # TODO: need to figure out why this adds 2 blank rows after delete
-        table = self.active_table()
-        e = table.model_from_activerow()
-        row = table.row_from_activerow()
+        view = self.active_table()
+        e = view.model_from_activerow()
+        row = view.row_from_activerow()
         if row is None: return
 
         m = dict(Unit=e.Unit, DateAdded=e.DateAdded, Title=e.Title)
@@ -343,8 +236,7 @@ class MainWindow(QMainWindow):
         msg = f'Are you sure you would like to permanently delete the event:\n\n{f.pretty_dict(m)}'
         if dlgs.msgbox(msg=msg, yesno=True):
             row.update(delete=True)
-            table.model.removeRows(i=row.i)
-            # table.model.removeRow(row.i)
+            view.model().removeRows(i=row.i)
 
 class TabWidget(QTabWidget):
     def __init__(self, parent):
@@ -352,9 +244,10 @@ class TabWidget(QTabWidget):
         self.tabindex = dd(int)
         
         m = f.config['TableName']['Class'] # get list of table classes from config
-        
+        lst = ['EventLog', 'WorkOrders', 'TSI', 'ComponentCO', 'UnitInfo', 'FCSummary', 'FCDetails', 'EmailList', 'Availability']
+       
         # Add tabs to widget
-        for i, title in enumerate(m):
+        for i, title in enumerate(lst):
             self.addTab(getattr(tbls, title)(parent=self), m[title])
             self.tabindex[m[title]] = i
         
@@ -374,11 +267,6 @@ def launch():
     w = MainWindow()
     # disable_window_animations_mac(w)
 
-    # monitor_num = 1 if f.is_win() else 0
-
-    # monitor = QDesktopWidget().screenGeometry(monitor_num)
-    # w.move(monitor.left(), monitor.top())
-    # w.showMaximized()
     w.show()
     w.after_init()
     return app.exec_()
