7f827dd4b063955fcb6d2e06174f2b35d7f3b277
Jayme Gordon <jaymegordo@gmail.com>
2020-06-09 11:15:34 -0700

    Added funcs to manually add dt exclusions

--- smseventlog/availability.py
+++ smseventlog/availability.py
@@ -7,11 +7,13 @@ from pypika import Case, Criterion
 from pypika import CustomFunction as cf
 from pypika import Order
 from pypika import functions as fn
+import sqlalchemy as sa
 
 from . import emails as em
 from . import functions as f
 from .__init__ import *
 from .database import db
+from . import dbmodel as dbm
 
 
 def import_single(p):
@@ -29,10 +31,31 @@ def import_dt_exclusions_email():
     maxdate = db.max_date_db(table='DowntimeExclusions', field='Date') + delta(days=2)
     df = em.combine_email_data(folder='Downtime', maxdate=maxdate, subject='Equipment Availability', header=0)  
     df = process_df_exclusions(df=df)
-    return df
+    import_dt_exclusions(df=df)
+
+def import_dt_exclusions(df):
     db.import_df(df=df, imptable='DowntimeExclusionsImport', impfunc='ImportDowntimeExclusions')
 
+def create_dt_exclusions(dates, rng_unit):
+    # manually create exclusion hrs when emails didn't get sent
+    # dates is list of dates
+    df = pd.concat([create_dt_exclusions_single(date, rng_unit) for date in dates])
+    return df
+
+def create_dt_exclusions_single(date, rng_unit, hrs=24):
+    df = pd.DataFrame(columns=['Unit', 'Date', 'Hours', 'MA'])
+    df.Unit = range_units(rng_unit)
+    df.Hours = hrs
+    df.MA = 1
+    df.Date = date
+    return df
+
+def range_units(rng):
+    # rng is tuple of first-last units eg (317, 330)
+    return tuple([f'F{unit}' for unit in range(rng[0], rng[1] + 1)])
+
 def process_df_exclusions(df):
+    # convert csv from suncor to database format
     if df is None: return None
 
     df = df[['EqmtUnit', 'TC08', 'Duration', 'DateEmail']]
@@ -44,6 +67,27 @@ def process_df_exclusions(df):
     df['MA'] = 1 # assume all exclusions always apply to MA and PA, will have to manually change when needed
     df.drop(columns=['OutOfSystem', 'Total'], inplace=True)
     return df
+
+def update_dt_exclusions_ma(units, rng_dates=None, dates=None):
+    # set MA=0 for units in given date range range
+
+    t = pk.Table('DowntimeExclusions')
+    cond = []
+    cond.append(t.Unit.isin(units))
+
+    if not rng_dates is None:
+        cond.append(t.Date.between(*rng_dates))
+    
+    if not dates is None:
+        cond.append(t.Date.isin(dates))
+
+    q = pk.Query().update(t).set(t.MA, 0).where(pk.Criterion.all(cond))
+    sql = q.get_sql()
+    
+    # print(sql)
+    cursor = db.get_cursor()
+    cursor.execute(sql)
+    cursor.commit()
 
 def process_df_downtime(df):
     if df is None: return None
@@ -65,6 +109,7 @@ def process_df_downtime(df):
     df.columns = ['Unit', 'StartDate', 'EndDate', 'Duration', 'DownReason', 'Comment', 'ShiftDate', 'Origin']
 
     df.Unit = df.Unit.str.replace('F0', 'F')
+    df.drop_duplicates(subset=['Unit', 'StartDate', 'EndDate'], inplace=True)
 
     return df
 
