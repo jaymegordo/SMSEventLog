2892043cfe3fe6f8207d2a41eb4b57654c2c03ec
Jayme Gordon <jaymegordo@gmail.com>
2020-06-16 18:43:20 -0700

    added fields for CurrentCompSMR, life remaining

--- SQL/ComponentChangeout/Component SMR.sql
+++ SQL/ComponentChangeout/Component SMR.sql
@@ -15,19 +15,56 @@ GO
 ALTER VIEW [dbo].[viewPredictedCO]
 AS 
 
-    SELECT t3.MineSite, t3.Model, t3.Unit, t3.Component, t3.Modifier, t3.BenchSMR, t3.CurrentUnitSMR, t2.LastCO, 
-        DateAdd(day, (t3.BenchSMR-(t3.CurrentUnitSMR-ISNULL(t2.LastCO,0)))/20, CURRENT_TIMESTAMP) as PredictedCODate
-    FROM (
-        SELECT u.MineSite, u.Model, u.Unit, c.Component, c.Modifier, c.Floc, c.BenchSMR, t1.CurrentUnitSMR 
-        FROM ComponentType c, (
-            SELECT u2.Unit, -1*(DateDiff(day, CURRENT_TIMESTAMP, Max(s.DateSMR))*20)+Max(s.SMR) AS CurrentUnitSMR
-            FROM UnitID u2 INNER JOIN UnitSMR s ON u2.Unit=s.Unit
-            GROUP BY u2.Unit) t1 INNER JOIN (UnitID u INNER JOIN EquipType e on u.Model=e.Model) ON t1.Unit=u.Unit
-        Where u.Active=1 and c.BenchSMR is not Null and c.Major=1 and c.EquipClass=e.EquipClass) as t3 
+SELECT 
+    t4.*,
+    DATEDIFF(day, CURRENT_TIMESTAMP, t4.PredictedCODate) as LifeRemaining
+FROM 
+    (SELECT
+        t3.MineSite,
+        t3.Model,
+        t3.Unit,
+        t3.Component,
+        t3.Modifier,
+        t3.BenchSMR,
+        t3.CurrentUnitSMR,
+        t2.LastCO as SMRLastCO, 
+        t3.CurrentUnitSMR - ISNULL(t2.LastCO, 0) as CurrentComponentSMR,
+        CAST(DATEADD(day, (t3.BenchSMR - (t3.CurrentUnitSMR - ISNULL(t2.LastCO, 0))) / 20, CURRENT_TIMESTAMP) as DATE) as PredictedCODate
+    
+    FROM 
+        (SELECT 
+            u.MineSite,
+            u.Model,
+            u.Unit,
+            c.Component,
+            c.Modifier,
+            c.Floc,
+            c.BenchSMR,
+            t1.CurrentUnitSMR
+
+        FROM ComponentType c, 
+            (SELECT
+                u2.Unit,
+                -1 * (DATEDIFF(day, CURRENT_TIMESTAMP, MAX(s.DateSMR)) * 20) + Max(s.SMR) AS CurrentUnitSMR
+
+            FROM UnitID u2
+                INNER JOIN UnitSMR s ON u2.Unit=s.Unit
+            GROUP BY u2.Unit) t1
+            
+            INNER JOIN (UnitID u 
+                INNER JOIN EquipType e on u.Model=e.Model) ON t1.Unit=u.Unit
+        
+        WHERE 
+            u.Active=1 and
+            c.BenchSMR IS NOT NULL and
+            c.Major=1 and
+            c.EquipClass=e.EquipClass) as t3 
+
         LEFT JOIN (
             SELECT EventLog.Unit, EventLog.Floc, Max(EventLog.SMR) AS LastCO 
             FROM EventLog
             WHERE ComponentCO='True'
             GROUP BY EventLog.Unit, EventLog.Floc) AS t2 ON t2.Floc=t3.Floc AND t2.Unit=t3.Unit
+    ) as t4
     -- ORDER BY t3.Unit, t3.Floc
 GO
