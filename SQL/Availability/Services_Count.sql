44e17659401f190cc9c0c3fec4c163a1db2f559f
Jayme Gordon <jaymegordo@gmail.com>
2020-08-10 14:14:01 -0700

    fixed formatting

--- SQL/Availability/Services_Count.sql
+++ SQL/Availability/Services_Count.sql
@@ -6,26 +6,41 @@ RETURNS TABLE
 AS
 RETURN
 
-With C1 AS (
-    Select a.Unit, CategoryAssigned, Duration, StartDate, EndDate, 
+WITH c1 as (
+    SELECT 
+        a.Unit,
+        a.CategoryAssigned,
+        a.Duration,
+        a.StartDate,
+        a.EndDate, 
         CASE
-            WHEN DATEDIFF(Hour, LAG(StartDate, 1) OVER (Partition By a.Unit Order By a.Unit, StartDate), StartDate) <= 48 THEN 0
+            WHEN DATEDIFF(Hour, LAG(a.StartDate, 1) OVER (PARTITION BY a.Unit ORDER BY a.Unit, a.StartDate), a.StartDate) <= 48 THEN 0
             ELSE 1
-        END As isstart
-    From Downtime a Inner Join UnitID b On a.Unit=b.Unit
-    Where CategoryAssigned Like '%Service%' and StartDate>=@DateLower and StartDate < DATEADD(DAY, -7, CONVERT(DATE, GETDATE())) and b.MineSite='FortHills'
+        END as isstart
+
+    FROM Downtime a INNER JOIN UnitID b ON a.Unit=b.Unit
+    WHERE
+        a.CategoryAssigned Like '%Service%' and
+        a.StartDate >= @DateLower and
+        a.StartDate < DATEADD(DAY, -7, CONVERT(DATE, GETDATE())) and
+        b.MineSite = 'FortHills'
 ),
-C2 AS (
-    Select *,
-        SUM(isstart) OVER (Partition By Unit Order By Unit, StartDate ROWS UNBOUNDED PRECEDING) As grp
-    From C1
+c2 as (
+    SELECT
+        *,
+        SUM(c1.isstart) OVER (PARTITION BY c1.Unit ORDER BY c1.Unit, c1.StartDate ROWS UNBOUNDED PRECEDING) as grp
+    From c1
 ), 
-C3 AS (
-    Select Unit, CategoryAssigned, SUM(Duration) as Duration, grp
-    From C2
-    Group By Unit, CategoryAssigned, grp
+c3 as (
+    SELECT
+        c2.Unit,
+        c2.CategoryAssigned,
+        SUM(c2.Duration) as Duration,
+        c2.grp
+    FROM c2
+    GROUP BY c2.Unit, c2.CategoryAssigned, c2.grp
 )
-Select Unit, SUM(CASE WHEN CategoryAssigned='S4 Service' THEN 1 ELSE 0 END) As S4, SUM(CASE WHEN CategoryAssigned='S5 Service' THEN 1 ELSE 0 END) As S5 
-From C3
-Group By Unit
+SELECT c3.Unit, SUM(CASE WHEN c3.CategoryAssigned='S4 Service' THEN 1 ELSE 0 END) as S4, SUM(CASE WHEN c3.CategoryAssigned='S5 Service' THEN 1 ELSE 0 END) as S5 
+From c3
+Group By c3.Unit
 ;
