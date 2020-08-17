bb9831e9d674042a8912f9f04589ccfa6dc686ad
Jayme Gordon <jaymegordo@gmail.com>
2020-08-17 08:52:51 -0700

    Updated set_pics, add func to fix folder path

--- smseventlog/gui/eventfolders.py
+++ smseventlog/gui/eventfolders.py
@@ -6,7 +6,8 @@ from .__init__ import *
 log = logging.getLogger(__name__)
 
 class EventFolder(object):
-    def __init__(self, e, model=None, irow=None):
+    def __init__(self, e, table_model=None, irow=None, table_widget=None):
+        # table_model only needed to set pics in table view
         self.dt_format = '%Y-%m-%d'
 
         f.copy_model_attrs(model=e, target=self)
@@ -42,6 +43,23 @@ class EventFolder(object):
         self.check()
         return self._p_event
 
+    def update_eventfolder_path(self, vals):
+        # update folder path with defaults (actually new vals) + changed vals (previous)
+        m_prev = dict(
+            unit=self.unit,
+            dateadded=self.dateadded,
+            workorder=self.workorder,
+            title=self.title)
+
+        m_prev.update(vals)
+
+        p_prev = self.p_base / self.get_folder_title(**m_prev)
+        if not p_prev == self._p_event:
+            self.check(p_prev=p_prev)
+
+            if not self.table_widget is None:
+                self.table_widget.mainwindow.update_statusbar(msg=f'Folder path updated: {self.folder_title}')
+
     def get_modelbase(self):
         # get modelbase from db for units in Regional
         # TODO this probably needs to move somewhere else eventually
@@ -49,11 +67,11 @@ class EventFolder(object):
         q = t.select(t.ModelBase).where(t.Model==self.model)
         return db.query_single_val(q)
 
-    def get_folder_title(self, unit, d, wo, title):
-        d_str = d.strftime(self.dt_format)
-        return f'{unit} - {d_str} - {wo} - {title}'
+    def get_folder_title(self, unit, dateadded, workorder, title):
+        d_str = dateadded.strftime(self.dt_format)
+        return f'{unit} - {d_str} - {workorder} - {title}'
 
-    def check(self):
+    def check(self, p_prev=None):
         if not fl.drive_exists():
             return
             
@@ -61,7 +79,10 @@ class EventFolder(object):
         p_blank = self.p_event_blank
 
         if not p.exists():
-            if p_blank.exists():
+            if not p_prev is None and p_prev.exists():
+                # rename folder when title changed
+                fl.move_folder(p_src=p_prev, p_dst=p)
+            elif p_blank.exists():
                 # if wo_blank stills exists but event now has a WO, automatically rename
                 fl.move_folder(p_src=p_blank, p_dst=p)
             else:
@@ -92,16 +113,24 @@ class EventFolder(object):
     
     def set_pics(self):
         # count number of pics in folder and set model + save to db
-        model, irow = self.model, self.irow
-        if model is None or irow is None: return # need model to set value
-        # TODO need to check and set pics from any table, have to bypass using model
-        if model.table_widget.title != 'Work Orders': return
-
+        model, irow = self.table_model, self.irow
+        
         p_pics = self._p_event / 'Pictures'
         num_pics = fl.count_files(p=p_pics, ftype='pics')
+        if hasattr(self, 'pictures') and num_pics == self.pictures: return # same as previous pictures
 
-        index = model.createIndex(irow, model.get_col_idx('Pics'))
-        model.setData(index=index, val=num_pics)
+        # if WorkOrders table active, use setData to set table + db
+        if not model is None and model.table_widget.title == 'Work Orders':
+            if irow is None: return # need model/row to set value in table view
+
+            index = model.createIndex(irow, model.get_col_idx('Pics'))
+            model.setData(index=index, val=num_pics)
+        else:
+            # just set str8 to db with uid
+            row = Row(keys=dict(UID=self.uid), dbtable=dbm.EventLog)
+            row.update(vals=dict(Pictures=num_pics))
+            print(f'num pics updated in db: {num_pics}')
+
     
     def create_folder(self, show=True, ask_show=False):
         if not fl.drive_exists():
@@ -157,10 +186,12 @@ class FortHills(EventFolder):
         return f'Fort Hills/02. Equipment Files'
 
 class BaseMine(EventFolder):
+    # NOTE may need to add other models in here eg shovels?
+    trucks = '1. Trucks'
     model_map = {
-        '980': '2. 980E',
-        '930': '1. 930E',
-        'HD1500': '3. HD1500'}
+        '980': f'{trucks}/2. 980E',
+        '930': f'{trucks}/1. 930E',
+        'HD1500': f'{trucks}/3. HD1500'}
     
     def __init__(self, **kw):
         super().__init__(**kw)
@@ -180,3 +211,12 @@ class Elkford(EventFolder):
 def get_eventfolder(minesite=None):
     # Try to get minesite specific event folder, else use default
     return getattr(sys.modules[__name__], minesite, EventFolder)
+
+def example():
+    from . import startup
+    from .. import dbtransaction as dbt
+    app = startup.get_qt_app()
+    e = dbt.example()
+    efl = get_eventfolder(minesite=e.MineSite)(e=e)
+
+    return efl
