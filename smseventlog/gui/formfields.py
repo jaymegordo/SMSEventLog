58d889987851c15f8ec1e7f9ab01a13fe9ee5004
Jayme Gordon <jaymegordo@gmail.com>
2020-08-24 10:40:16 -0700

    set SpinBox to return None instead of 0

--- smseventlog/gui/formfields.py
+++ smseventlog/gui/formfields.py
@@ -179,3 +179,9 @@ class SpinBox(QSpinBox, FormFields):
         if not range is None:
             self.setRange(*range)
 
+    @FormFields.val.getter
+    def val(self):
+        # Always return None instead of 0
+        # NOTE this may need to change but is good for now
+        v = super().val
+        return v if not v == 0 else None
