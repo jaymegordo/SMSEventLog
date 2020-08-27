848bad24e28edcdef9dc2983c613ab4c128ab110
Jayme Gordon <jaymegordo@gmail.com>
2020-08-27 12:09:55 -0700

    Bump version: 3.0.6 → 3.1.0

--- smseventlog/__init__.py
+++ smseventlog/__init__.py
@@ -28,7 +28,7 @@ try:
 except ModuleNotFoundError:
     pass
 
-__version__ = '3.0.6'
+__version__ = '3.1.0'
 VERSION = __version__
 
 AZURE_ENV = os.getenv("AZURE_FUNCTIONS_ENVIRONMENT") # dont think these are used
