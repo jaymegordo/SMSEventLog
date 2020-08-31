164f697770fcba6836ec7bf9d5e2f3718514b413
Jayme Gordon <jaymegordo@gmail.com>
2020-08-31 10:33:06 -0700

    Bump version: 3.1.4 → 3.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from setuptools import Command, find_packages, setup
 
-VERSION = '3.1.4'
+VERSION = '3.1.5'
 
 # Remove leftover folders from setuptools
 class CleanCommand(Command):
