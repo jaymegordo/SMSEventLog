1f8195562e6d8a47254cc91cb1c5f82961ceebd8
Jayme Gordon <jaymegordo@gmail.com>
2020-11-10 15:56:32 -0800

    Clean up multithread err handling

--- smseventlog/gui/multithread.py
+++ smseventlog/gui/multithread.py
@@ -1,12 +1,10 @@
 import sys
-import traceback
-import logging
 
 from PyQt5.QtCore import QObject, QRunnable, pyqtSignal, pyqtSlot
-
-from .. import functions as f
-from .. import errors as er
 from smseventlog.__init__ import getlog
+
+from .. import errors as er
+from .. import functions as f
 
 log = getlog(__name__)
 
@@ -15,7 +13,7 @@ log = getlog(__name__)
 
 class WorkerSignals(QObject):
     finished = pyqtSignal()
-    error = pyqtSignal(str, str, object)
+    error = pyqtSignal(str, object)
     result = pyqtSignal(object)
     progress = pyqtSignal(int)
 
@@ -29,27 +27,34 @@ class Worker(QRunnable):
 
     @pyqtSlot()
     def run(self):
-
+        """Run task in background worker thread"""
         try:
             result = self.func(*self.args, **self.kw)
         except:
-            # exctype, value = sys.exc_info()[:2]
-            # self.signals.error.emit((exctype, value, traceback.format_exc()))
-            
-            # need to use logger internal to thread to properly capture stack trace
-            msg = f'Multithread Error - {self.func.__name__}' #\n{er.format_traceback()}'
-            log.error(msg, exc_info=True)
-
-            # also emit for gui to display
-            self.signals.error.emit('Multithread Error', er.format_traceback(), self.func)
+            msg = f'Multithread Error - {self.func.__name__}'
+            self.signals.error.emit(msg, sys.exc_info())
         else:
             self.signals.result.emit(result)  # Return the result of the processing
         finally:
             self.signals.finished.emit()  # Done
 
-    def add_signals(self, signals=None):
-        # add signals as list of tuples of:
-        # eg: ('signal', m_args) > eg dict(func=self._install_update, args=None, kw=dict(uid=uid))
+    def add_signals(self, signals: list=None):
+        """Add signals 
+        
+        Parameters
+        ----------
+        signals : list\n
+            list of tuples of ('signal_name', dict(**kw))
+        
+        Examples
+        -------
+        Worker.add_signals(
+            signals=('result', dict(func=self._install_update, args=None, kw=dict(uid=uid))
+
+        Returns
+        ------
+        Worker (self)
+        """
         if not isinstance(signals, list): signals = [signals]
 
         for sig, m in signals:
@@ -58,7 +63,7 @@ class Worker(QRunnable):
                 getattr(self.signals, sig).connect(
                     lambda x: m.get('func', None)(x, *m.get('args', ()), **m.get('kw', {})))
             except:
-                self.signals.error.emit(f'Failed to connect signal: {sig}, {m}', er.format_traceback())
+                self.signals.error.emit(f'Failed to connect signal: {sig}, {m}', sys.exc_info())
 
         return self
 
@@ -69,13 +74,6 @@ class Worker(QRunnable):
         else:
             log.error('Multithread Worker has no mainwindow or threadpool to start.', exc_info=True)
     
-def send_error(msg, tb_msg=None, func=None):
-    """wrapper to send **kw args to er.log_error from a signal in different thread
-    This error handling is so messy I'm sorry future me or someone else.."""
-    if tb_msg is None:
-        tb_msg = traceback.format_exc()
-    
-    if not func is None:
-        msg = f'{msg} - {func.__name__}'
-    
-    er.log_error(msg=msg, tb_msg=tb_msg, log=None, display=True, func=func)
+def send_error(msg=None, exc_info=None):
+    """Wrapper to send **kw args to er.log_error from a signal in different thread"""   
+    er.log_error(msg=msg, exc_info=exc_info, display=True)
