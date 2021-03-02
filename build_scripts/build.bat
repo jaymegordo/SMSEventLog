756eea802509d23b18f4e68a0369fabff500d3ed
Jayme Gordon <jaymegordo@gmail.com>
2021-03-01 17:40:39 -0800

    Bump version: 3.4.0a1 → 3.4.0a2

--- build_scripts/build.bat
+++ build_scripts/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set upload=%1
-set version="3.4.0a1"
+set version="3.4.0a2"
 
 echo version=%version%, upload=%upload%
 
--- smseventlog/__init__.py
+++ smseventlog/__init__.py
@@ -27,7 +27,7 @@ from pypika import functions as fn
 from pypika.analytics import RowNumber
 
 
-__version__ = '3.4.0a1'
+__version__ = '3.4.0a2'
 VERSION = __version__
 
 # Set environments
